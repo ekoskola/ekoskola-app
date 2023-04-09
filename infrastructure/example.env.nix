7030c9df0dd8c0674cfad65684c0bd920d2d51f1
ekoskola <xx@xxx.com>
2023-04-09 16:35:42 +0200

    feat(infrastructure): add postrgres

--- infrastructure/example.env.nix
+++ infrastructure/example.env.nix
@@ -1,4 +1,9 @@
 {
   HOST_IP = "111.11.111.11";
   ACME_EMAIL = "test@email.com";
+  EKOSKOLA_FRONTEND_NGINX_ROOT = "/var/www/ekoskola/build";
+  EKOSKOLA_SERVER_NGINX_HOST = "test.com";
+
+  EKOSKOLA_DB_NAME = "ekoskola";
+  EKOSKOLA_DB_PASSWORD = "password";
 }
--- infrastructure/network.nix
+++ infrastructure/network.nix
@@ -19,28 +19,26 @@ let envHelper = import ./env.nix; in
 
     networking.firewall.allowedTCPPorts = [ 80 443 ];
 
-    services.postgresql.enable = true;
-
     #   "/var/www/ekoskola".source = builtins.fetchGit {
     #     url = "https://github.com/ekoskola/ekoskola-app";
     #     ref = "master";
     #   };
 
-    # services.postgresql = {
-    #   enable = true;
-    #   package = pkgs.postgresql_15;
-    #   enableTCPIP = true;
-    #   authentication = pkgs.lib.mkOverride 10 ''
-    #     local all all trust
-    #     host all all 127.0.0.1/32 trust
-    #     host all all ::1/128 trust
-    #   '';
-    #   initialScript = pkgs.writeText "backend-initScript" ''
-    #     CREATE ROLE nixcloud WITH LOGIN PASSWORD 'nixcloud' CREATEDB;
-    #     CREATE DATABASE nixcloud;
-    #     GRANT ALL PRIVILEGES ON DATABASE nixcloud TO nixcloud;
-    #   '';
-    # };
+    services.postgresql = {
+      enable = true;
+      # package = pkgs.postgresql_15;
+      enableTCPIP = true;
+      authentication = pkgs.lib.mkOverride 10 ''
+        local all all trust
+        host all all 127.0.0.1/32 trust
+        host all all ::1/128 trust
+      '';
+      initialScript = pkgs.writeText "backend-initScript" ''
+        CREATE ROLE ${envHelper.EKOSKOLA_DB_NAME} WITH LOGIN PASSWORD '${envHelper.EKOSKOLA_DB_PASSWORD}' CREATEDB;
+        CREATE DATABASE ${envHelper.EKOSKOLA_DB_NAME};
+        GRANT ALL PRIVILEGES ON DATABASE ${envHelper.EKOSKOLA_DB_NAME} TO ${envHelper.EKOSKOLA_DB_NAME};
+      '';
+    };
 
     systemd.services.ekoskolaServer = {
       description = "Server for ekoskola app";
@@ -58,16 +56,23 @@ let envHelper = import ./env.nix; in
       enable = true;
       recommendedProxySettings = true;
       recommendedTlsSettings = true;
-      virtualHosts."${envHelper.EKOSKOLA_FRONTEND_NGINX_HOST}" = {
-        default = true;
+      virtualHosts."${envHelper.EKOSKOLA_SERVER_NGINX_HOST}" = {
         addSSL = true;
         enableACME = true;
-        root = envHelper.EKOSKOLA_FRONTEND_NGINX_ROOT;
         locations."/" = {
-          # https://stackoverflow.com/questions/43951720/react-router-and-nginx
-          tryFiles = "$uri /index.html";
+          proxyPass = "http://127.0.0.1:8000";
         };
       };
+      # virtualHosts."${envHelper.EKOSKOLA_FRONTEND_NGINX_HOST}" = {
+      #   default = true;
+      #   addSSL = true;
+      #   enableACME = true;
+      #   root = envHelper.EKOSKOLA_FRONTEND_NGINX_ROOT;
+      #   locations."/" = {
+      #     # https://stackoverflow.com/questions/43951720/react-router-and-nginx
+      #     tryFiles = "$uri /index.html";
+      #   };
+      # };
     };
 
     security.acme = {
@@ -83,7 +88,7 @@ let envHelper = import ./env.nix; in
         {
           scheme = "https";
           port = 443;
-          host = envHelper.EKOSKOLA_FRONTEND_NGINX_HOST;
+          host = envHelper.EKOSKOLA_SERVER_NGINX_HOST;
           path = "/";
           description = "Check that ekoskola is running.";
         }
